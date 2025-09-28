1991837955af16cc4a37149ff8c9cfb828e804ca
Riley Rivera <rileyanticode@gmail.com>
2025-09-28 00:56:34 -0700

    Require Better Combat

--- src/main/java/me/anticode/abco/BCOverhauls.java
+++ src/main/java/me/anticode/abco/BCOverhauls.java
@@ -1,10 +1,12 @@
 package me.anticode.abco;
 
+import net.bettercombat.BetterCombat;
 import net.fabricmc.api.ModInitializer;
 
 public class BCOverhauls implements ModInitializer {
 
     @Override
     public void onInitialize() {
+
     }
 }
