69c0445db6c3c0664d9527f23b191ab39d15f808
Riley Rivera <rileyanticode@gmail.com>
2025-09-28 00:39:21 -0700

    Initial commit

---
+++ src/main/java/me/anticode/abco/BCOverhauls.java
@@ -0,0 +1,10 @@
+package me.anticode.abco;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BCOverhauls implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/anticode/abco/client/BCOverhaulsClient.java
@@ -0,0 +1,10 @@
+package me.anticode.abco.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BCOverhaulsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
