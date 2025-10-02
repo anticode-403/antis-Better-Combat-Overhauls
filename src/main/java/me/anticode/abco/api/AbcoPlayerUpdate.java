47ce7f1e8fe5f0f4f109d85d990ebc10b963a035
Riley Rivera <rileyanticode@gmail.com>
2025-10-01 20:29:49 -0700

    Convert AbcoPlayerUpdate to record

--- src/main/java/me/anticode/abco/api/AbcoPlayerUpdate.java
+++ src/main/java/me/anticode/abco/api/AbcoPlayerUpdate.java
@@ -2,22 +2,6 @@ package me.anticode.abco.api;
 
 import net.bettercombat.logic.AnimatedHand;
 
-public class AbcoPlayerUpdate {
-    public final int playerId;
-    public final int comboCount;
-    public final boolean heavyAttack;
-    public final AnimatedHand animatedHand;
-    public final String animationName;
-    public final float length;
-    public final float upswing;
-
-    public AbcoPlayerUpdate(int id, boolean heavy, int combo, AnimatedHand animatedHand, String animation, float length, float upswing) {
-        this.playerId = id;
-        this.comboCount = combo;
-        this.heavyAttack = heavy;
-        this.animatedHand = animatedHand;
-        this.animationName = animation;
-        this.length = length;
-        this.upswing = upswing;
-    }
+public record AbcoPlayerUpdate(int playerId, boolean heavyAttack, int comboCount, AnimatedHand animatedHand,
+                               String animationName, float length, float upswing) {
 }
--- src/main/java/me/anticode/abco/network/AbcoServerNetwork.java
+++ src/main/java/me/anticode/abco/network/AbcoServerNetwork.java
@@ -7,7 +7,6 @@ import me.anticode.abco.api.HeavyAttackComboApi;
 import net.bettercombat.network.Packets;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.world.ServerWorld;
 
 public class AbcoServerNetwork {
@@ -16,7 +15,7 @@ public class AbcoServerNetwork {
             ServerWorld world = Iterables.tryFind(server.getWorlds(), (element) -> element == player.getWorld()).orNull();
             if (world == null || world.isClient) return;
             final AbcoPlayerUpdate packet = AbcoPackets.C2S_PlayerUpdaterRequest.read(buf);
-            final var forwardBuffer = new Packets.AttackAnimation(player.getId(), packet.animatedHand, packet.animationName, packet.length, packet.upswing).write();
+            final var forwardBuffer = new Packets.AttackAnimation(player.getId(), packet.animatedHand(), packet.animationName(), packet.length(), packet.upswing()).write();
             try {
                 //send info back for Replaymod Compat
                 if (ServerPlayNetworking.canSend(player, Packets.AttackAnimation.ID)) {
@@ -37,8 +36,8 @@ public class AbcoServerNetwork {
             world.getServer().executeSync(() -> {
                 ABCOPlayerEntity abcoPlayerEntity = (ABCOPlayerEntity)player;
                 HeavyAttackComboApi comboPlayer = (HeavyAttackComboApi)player;
-                if (packet.heavyAttack) {
-                    comboPlayer.antisBetterCombatOverhauls$setHeavyCombo(packet.comboCount);
+                if (packet.heavyAttack()) {
+                    comboPlayer.antisBetterCombatOverhauls$setHeavyCombo(packet.comboCount());
                     abcoPlayerEntity.antisBetterCombatOverhauls$setLastAttackSpecial(true);
                 } else {
                     abcoPlayerEntity.antisBetterCombatOverhauls$setLastAttackSpecial(false);
